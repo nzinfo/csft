nzinfo/csft

commit 25ac58be7f3a1040c6b831ca4bbd4ef3567703df

proper sorting by @expr (no more reinterpreting as int) proper sorting by @expr vs filters (explicit early/late calc phases) added support for filtering and sorting settings to test framework
git-svn-id: http://sphinxsearch.googlecode.com/svn/trunk@1152 8b96e2b9-35c5-2c16-bc47-5122d61876d4

src/sphinx.cpp
@@ -1666,9 +1666,11 @@ private:
 	bool						MatchExtendedV1 ( const CSphQuery * pQuery, CSphQueryResult * pResult, int iSorters, ISphMatchSorter ** ppSorters, const CSphTermSetup & tTermSetup );
 	void						MatchFullScan ( const CSphQuery * pQuery, int iSorters, ISphMatchSorter ** ppSorters, const CSphTermSetup & tTermSetup );
 
-	const DWORD *				FindDocinfo ( SphDocID_t uDocID );
-	void						CalcDocinfo ( CSphMatch & tMatch, const DWORD * pDocinfo );
-	void						LookupDocinfo ( CSphMatch & tMatch );
+	const DWORD *				FindDocinfo ( SphDocID_t uDocID ) const;
+	void						CopyDocinfo ( CSphMatch & tMatch, const DWORD * pFound ) const;
+	void						EarlyCalc ( CSphMatch & tMatch ) const;
+	void						LateCalc ( CSphMatch & tMatch ) const;
+	bool						EarlyReject ( CSphMatch & tMatch, const CSphQuery * pQuery ) const;
 
 	bool						BuildMVA ( const CSphVector<CSphSource*> & dSources, CSphAutoArray<CSphWordHit> & dHits, int iArenaSize, int iFieldFD, int nFieldMVAs, int iFieldMVAInPool );
 
@@ -8028,13 +8030,17 @@ CSphQueryResult * CSphIndex_VLN::Query ( ISphTokenizer * pTokenizer, CSphDict * 
 }
 
 
-static inline bool sphMatchEarlyReject ( const CSphMatch & tMatch, const CSphQuery * pQuery, CSphSharedBuffer<DWORD> & pMvaStorage )
+bool CSphIndex_VLN::EarlyReject ( CSphMatch & tMatch, const CSphQuery * pQuery ) const
 {
 	if ( tMatch.m_iDocID < pQuery->m_iMinID || tMatch.m_iDocID > pQuery->m_iMaxID )
 		return true;
 
 	if ( !pQuery->m_dFilters.GetLength() )
 		return false;
+
+	if ( m_bEarlyLookup )
+		CopyDocinfo ( tMatch, FindDocinfo ( tMatch.m_iDocID ) );
+	EarlyCalc ( tMatch );
 
 	ARRAY_FOREACH ( i, pQuery->m_dFilters )
 	{
@@ -8051,7 +8057,7 @@ static inline bool sphMatchEarlyReject ( const CSphMatch & tMatch, const CSphQue
 			const DWORD * pMvaMax = NULL;
 			if ( uOff )
 			{
-				pMva = &pMvaStorage [ uOff ];
+				pMva = &m_pMva [ uOff ];
 				pMvaMax = pMva + (*pMva) + 1;
 				pMva++;
 			}
@@ -8144,7 +8150,7 @@ static inline bool sphMatchEarlyReject ( const CSphMatch & tMatch, const CSphQue
 }
 
 
-const DWORD * CSphIndex_VLN::FindDocinfo ( SphDocID_t uDocID )
+const DWORD * CSphIndex_VLN::FindDocinfo ( SphDocID_t uDocID ) const
 {
 	if ( m_uDocinfo<=0 )
 		return NULL;
@@ -8210,19 +8216,25 @@ const DWORD * CSphIndex_VLN::FindDocinfo ( SphDocID_t uDocID )
 }
 
 
+void CSphIndex_VLN::CopyDocinfo ( CSphMatch & tMatch, const DWORD * pFound ) const
+{
+	if ( pFound )
+	{
+		assert ( tMatch.m_pRowitems );
+		assert ( DOCINFO2ID(pFound)==tMatch.m_iDocID );
+		memcpy ( tMatch.m_pRowitems, DOCINFO2ATTRS(pFound), m_tSchema.GetRowSize()*sizeof(CSphRowitem) );
+	}
+}
+
+
 static inline double sphSqr ( double v )
 {
 	return v*v;
 }
 
 
-void CSphIndex_VLN::CalcDocinfo ( CSphMatch & tMatch, const DWORD * pInfo )
+void CSphIndex_VLN::EarlyCalc ( CSphMatch & tMatch ) const
 {
-	assert ( tMatch.m_pRowitems );
-//	assert ( tMatch.m_iRowitems==m_tSchema.GetRowSize() + ( m_bCalcGeodist ? 1 : 0 ) );
-	assert ( DOCINFO2ID(pInfo)==tMatch.m_iDocID );
-	memcpy ( tMatch.m_pRowitems, DOCINFO2ATTRS(pInfo), m_tSchema.GetRowSize()*sizeof(CSphRowitem) );
-
 	if ( m_iCalcGeodist>=0 )
 	{
 		const double R = 6384000;
@@ -8234,7 +8246,11 @@ void CSphIndex_VLN::CalcDocinfo ( CSphMatch & tMatch, const DWORD * pInfo )
 		double c = 2*asin ( Min ( 1, sqrt(a) ) );
 		tMatch.SetAttrFloat ( m_iCalcGeodist, float(R*c) );
 	}
+}
 
+
+void CSphIndex_VLN::LateCalc ( CSphMatch & tMatch ) const
+{
 	if ( m_iCalcExpr>=0 )
 	{
 		float fRes = m_tCalcExpr.Eval ( tMatch );
@@ -8243,17 +8259,10 @@ void CSphIndex_VLN::CalcDocinfo ( CSphMatch & tMatch, const DWORD * pInfo )
 }
 
 
-void CSphIndex_VLN::LookupDocinfo ( CSphMatch & tMatch )
-{
-	const DWORD * pFound = FindDocinfo ( tMatch.m_iDocID );
-	if ( pFound )
-		CalcDocinfo ( tMatch, pFound );
-}
-
-
 #define SPH_SUBMIT_MATCH(_match) \
 	if ( m_bLateLookup ) \
-		LookupDocinfo ( _match ); \
+		CopyDocinfo ( _match, FindDocinfo ( (_match).m_iDocID ) ); \
+	LateCalc ( _match ); \
 	\
 	if ( bRandomize ) \
 		(_match).m_iWeight = rand(); \
@@ -8368,9 +8377,7 @@ void CSphIndex_VLN::MatchAll ( const CSphQuery * pQuery, CSphQueryResult * pResu
 		CSphMatch & tMatch = m_dQueryWords[0].m_tDoc;
 
 		// early reject by group id, doc id or timestamp
-		if ( m_bEarlyLookup )
-			LookupDocinfo ( tMatch );
-		if ( sphMatchEarlyReject ( tMatch, pQuery, m_pMva ) )
+		if ( EarlyReject ( tMatch, pQuery ) )
 		{
 			docID++;
 			i = 0;
@@ -8586,9 +8593,7 @@ void CSphIndex_VLN::MatchAny ( const CSphQuery * pQuery, CSphQueryResult * pResu
 
 		// early reject by group id, doc id or timestamp
 		CSphMatch & tMatch = m_dQueryWords[iMinIndex].m_tDoc;
-		if ( m_bEarlyLookup )
-			LookupDocinfo ( tMatch );
-		if ( sphMatchEarlyReject ( tMatch, pQuery, m_pMva ) )
+		if ( EarlyReject ( tMatch, pQuery ) )
 			continue;
 
 		// get the words we're matching current document against (let's call them "terms")
@@ -9018,9 +9023,7 @@ bool CSphIndex_VLN::MatchBoolean ( const CSphQuery * pQuery, CSphQueryResult * p
 		iMinID = 1 + pMatch->m_iDocID;
 
 		// early reject by group id, doc id or timestamp
-		if ( m_bEarlyLookup )
-			LookupDocinfo ( *pMatch );
-		if ( sphMatchEarlyReject ( *pMatch, pQuery, m_pMva ) )
+		if ( EarlyReject ( *pMatch, pQuery ) )
 			continue;
 
 		// set weight and push it to the queue
@@ -9944,10 +9947,7 @@ bool CSphIndex_VLN::MatchExtendedV1 ( const CSphQuery * pQuery, CSphQueryResult 
 			continue;
 
 		// early reject by group id, doc id or timestamp
-		if ( m_bEarlyLookup )
-			LookupDocinfo ( *pAccept );
-
-		if ( sphMatchEarlyReject ( *pAccept, pQuery, m_pMva ) )
+		if ( EarlyReject ( *pAccept, pQuery ) )
 			continue;
 
 		// submit
@@ -11780,10 +11780,7 @@ bool CSphIndex_VLN::MatchExtended ( const CSphQuery * pQuery, CSphQueryResult * 
 			CSphMatch & tMatch = pRoot->m_dMatches[i];
 
 			// early reject by group id, doc id or timestamp
-			if ( m_bEarlyLookup )
-				LookupDocinfo ( tMatch );
-
-			if ( sphMatchEarlyReject ( tMatch, pQuery, m_pMva ) )
+			if ( EarlyReject ( tMatch, pQuery ) )
 				continue;
 
 			// submit
@@ -11807,6 +11804,8 @@ void CSphIndex_VLN::MatchFullScan ( const CSphQuery * pQuery, int iSorters, ISph
 	CSphMatch tMatch;
 	tMatch.Reset ( tSetup.m_tMin.m_iRowitems + tSetup.m_iToCalc );
 	tMatch.m_iWeight = 1;
+
+	m_bEarlyLookup = false; // we'll do it manually
 
 	DWORD uStride = DOCINFO_IDSIZE + m_tSchema.GetRowSize();
 	for ( DWORD uIndexEntry=0; uIndexEntry<m_uDocinfoIndex; uIndexEntry++ )
@@ -11907,9 +11906,9 @@ void CSphIndex_VLN::MatchFullScan ( const CSphQuery * pQuery, int iSorters, ISph
 		for ( const DWORD * pDocinfo=pBlockStart; pDocinfo<=pBlockEnd; pDocinfo+=uStride )
 		{
 			tMatch.m_iDocID = DOCINFO2ID(pDocinfo);
-			CalcDocinfo ( tMatch, pDocinfo );
+			CopyDocinfo ( tMatch, pDocinfo );
 
-			if ( sphMatchEarlyReject ( tMatch, pQuery, m_pMva ) )
+			if ( EarlyReject ( tMatch, pQuery ) )
 				continue;
 
 			SPH_SUBMIT_MATCH ( tMatch );
@@ -13104,7 +13103,11 @@ bool CSphIndex_VLN::MultiQuery ( ISphTokenizer * pTokenizer, CSphDict * pDict, C
 			CSphMatch * const pTail = pHead + iCount;
 
 			for ( CSphMatch * pCur=pHead; pCur<pTail; pCur++ )
-				LookupDocinfo ( *pCur );
+			{
+				CopyDocinfo ( *pCur, FindDocinfo ( pCur->m_iDocID ) );
+				EarlyCalc ( *pCur );
+				LateCalc ( *pCur );
+			}
 		}
 
 		// mva ptr
@@ -17639,7 +17642,7 @@ void CSphDoclistRecord::Read( CSphMergeSource * pSource )
 	if ( pSource->m_bForceDocinfo )
 	{	
 		pSource->m_tMatch.m_iDocID = m_iDocID;
-		pSource->m_pIndex->LookupDocinfo ( pSource->m_tMatch );
+		pSource->m_pIndex->CopyDocinfo ( pSource->m_tMatch, pSource->m_pIndex->FindDocinfo ( pSource->m_tMatch.m_iDocID ) );
 	}
 
 	for ( int i=0; i<m_iRowitems; i++ )

src/sphinxsort.cpp
@@ -995,6 +995,25 @@ protected:
 	}
 };
 
+
+/// match sorter
+struct MatchExpr_fn : public ISphMatchComparator
+{
+	virtual bool VirtualIsLess ( const CSphMatch & a, const CSphMatch & b, const CSphMatchComparatorState & t ) const
+	{
+		return IsLess ( a, b, t );
+	}
+
+	static inline bool IsLess ( const CSphMatch & a, const CSphMatch & b, const CSphMatchComparatorState & t )
+	{
+		float aa = sphDW2F ( a.m_pRowitems [ t.m_iRowitem[0] ] );
+		float bb = sphDW2F ( b.m_pRowitems [ t.m_iRowitem[0] ] );
+		if ( aa!=bb )
+			return aa<bb;
+		return a.m_iDocID>b.m_iDocID;
+	}
+};
+
 /////////////////////////////////////////////////////////////////////////////
 
 #define SPH_TEST_PAIR(_aa,_bb,_idx ) \
@@ -1191,7 +1210,8 @@ enum ESphSortFunc
 	FUNC_GENERIC3,
 	FUNC_GENERIC4,
 	FUNC_GENERIC5,
-	FUNC_CUSTOM
+	FUNC_CUSTOM,
+	FUNC_EXPR
 };
 
 
@@ -1395,6 +1415,7 @@ static ISphMatchSorter * sphCreateSorter2nd ( ESphSortFunc eGroupFunc, bool bGro
 			case FUNC_GENERIC4:		return sphCreateSorter3rd<MatchGeneric4_fn<false> >	( bDistinct, bMVA, pComp, pQuery ); break;
 			case FUNC_GENERIC5:		return sphCreateSorter3rd<MatchGeneric5_fn<false> >	( bDistinct, bMVA, pComp, pQuery ); break;
 			case FUNC_CUSTOM:		return sphCreateSorter3rd<MatchCustom_fn<false>   >	( bDistinct, bMVA, pComp, pQuery ); break;
+			case FUNC_EXPR:			return sphCreateSorter3rd<MatchExpr_fn>				( bDistinct, bMVA, pComp, pQuery ); break;
 			default:				return NULL;
 		}
 	}
@@ -1431,6 +1452,7 @@ static ISphMatchSorter * sphCreateSorter1st ( ESphSortFunc eMatchFunc, bool bMat
 			case FUNC_GENERIC4:		pComp = new MatchGeneric4_fn<false>(); break;
 			case FUNC_GENERIC5:		pComp = new MatchGeneric5_fn<false>(); break;
 			case FUNC_CUSTOM:		pComp = new MatchCustom_fn<false>(); break;
+			case FUNC_EXPR:			pComp = new MatchExpr_fn(); break; // only for non-bitfields, obviously
 		}
 	}
 
@@ -1538,7 +1560,7 @@ ISphMatchSorter * sphCreateQueue ( CSphQuery * pQuery, const CSphSchema & tSchem
 		tStateMatch.m_iAttr[0] = FIXUP_EXPR;
 		tStateMatch.m_iAttr[1] = SPH_VATTR_ID;
 		tStateMatch.m_uAttrDesc = 1;
-		eMatchFunc = FUNC_GENERIC2;
+		eMatchFunc = FUNC_EXPR;
 		bUsesAttrs = true;
 
 	} else
@@ -1722,6 +1744,7 @@ ISphMatchSorter * sphCreateQueue ( CSphQuery * pQuery, const CSphSchema & tSchem
 				case FUNC_GENERIC4:	pTop = new CSphMatchQueue<MatchGeneric4_fn<false> >		( pQuery->m_iMaxMatches, bUsesAttrs ); break;
 				case FUNC_GENERIC5:	pTop = new CSphMatchQueue<MatchGeneric5_fn<false> >		( pQuery->m_iMaxMatches, bUsesAttrs ); break;
 				case FUNC_CUSTOM:	pTop = new CSphMatchQueue<MatchCustom_fn<false> >		( pQuery->m_iMaxMatches, bUsesAttrs ); break;
+				case FUNC_EXPR:		pTop = new CSphMatchQueue<MatchExpr_fn>					( pQuery->m_iMaxMatches, bUsesAttrs ); break;
 				default:			pTop = NULL;
 			}
 		}
@@ -1734,7 +1757,7 @@ ISphMatchSorter * sphCreateQueue ( CSphQuery * pQuery, const CSphSchema & tSchem
 	if ( !pTop )
 	{
 		sError.SetSprintf ( "internal error: unhandled sorting mode (match-sort=%d, group=%d, group-sort=%d)",
-			pQuery->m_iGroupbyOffset>=0, eMatchFunc, eGroupFunc );
+			eMatchFunc, pQuery->m_iGroupbyOffset>=0, eGroupFunc );
 		return NULL;
 	}
 

test/helpers.inc
@@ -299,15 +299,14 @@ class SphinxConfig
 			// new and cool
 			foreach ( ChildrenArray ( $qs, "query" ) as $q )
 			{
+				$res = array ( "query"=>$q->nodeValue );
+
 				// parse query mode
 				$mode = 0;
 				$mode_s = $q->getAttribute("mode");
-
-				if ( !$mode_s )
+				switch ( $mode_s )
 				{
-					$mode_s = "(default)";
-				} else switch ( $mode_s )
-				{
+					case "":			$mode_s = "(default)"; break;
 					case "all":			$mode = SPH_MATCH_ALL; break;
 					case "any":			$mode = SPH_MATCH_ANY; break;
 					case "phrase":		$mode = SPH_MATCH_PHRASE; break;
@@ -317,9 +316,30 @@ class SphinxConfig
 						printf ( "$config_file: unknown matching mode '%s'\n", $mode_s );
 						return false;
 				}
+				$res["mode"] = $mode;
+				$res["mode_s"] = $mode_s;
+
+				// parse filter
+				$res["filter"] = $q->getAttribute("filter");
+				$res["filtervalue"] = $q->getAttribute("filtervalue" );
+
+				// parse sort mode and get clause
+				$sortmode = 0;
+				$sortmode_s = $q->getAttribute("sortmode");
+				switch ( $sortmode_s )
+				{
+					case "":			$sortmode_s = "(default)"; break;
+					case "expr":		$sortmode = SPH_SORT_EXPR; break;
+					default:
+						printf ( "$config_file: unknown sorting mode '%s'\n", $sortmode_s );
+						return false;
+				}
+				$res["sortmode"] = $sortmode;
+				$res["sortmode_s" ] = $sortmode_s;
+				$res["sortby"] = $q->getAttribute("sortby");
 
 				// add query
-				$this->_queries[] = array ( "query"=>$q->nodeValue, "mode"=>$mode, "mode_s"=>$mode_s );
+				$this->_queries[] = $res;
 			}
 
 		} else
@@ -373,9 +393,9 @@ class SphinxConfig
 				$results = 0;
 				if ( empty($this->_query_settings) )
 				{
-					if ( $qinfo["mode"] )
-						$cl->SetMatchMode ( $qinfo["mode"] );
-
+					if ( $qinfo["mode"] )		$cl->SetMatchMode ( $qinfo["mode"] );
+					if ( $qinfo["sortmode"] )	$cl->SetSortMode ( $qinfo["sortmode"], $qinfo["sortby"] );
+					if ( $qinfo["filter"] )		$cl->SetFilter ( $qinfo["filter"], array ( $qinfo["filtervalue"] ) );
 	            	$results = $cl->Query ( $query, $index );
 
             	} else

test/test_16/model.bin
@@ -1,0 +1,1 @@
+a:1:{i:0;a:2:{i:0;a:11:{s:5:"error";s:0:"";s:7:"warning";s:0:"";s:6:"status";i:0;s:6:"fields";a:1:{i:0;s:4:"body";}s:5:"attrs";a:2:{s:8:"group_id";i:1;s:5:"@expr";i:5;}s:7:"matches";a:3:{i:444;a:2:{s:6:"weight";s:1:"4";s:5:"attrs";a:2:{s:8:"group_id";s:1:"1";s:5:"@expr";d:4;}}i:111;a:2:{s:6:"weight";s:1:"1";s:5:"attrs";a:2:{s:8:"group_id";s:1:"1";s:5:"@expr";d:1;}}i:222;a:2:{s:6:"weight";s:1:"1";s:5:"attrs";a:2:{s:8:"group_id";s:1:"1";s:5:"@expr";d:1;}}}s:5:"total";s:1:"3";s:11:"total_found";s:1:"3";s:4:"time";s:5:"0.002";s:5:"words";a:2:{s:4:"test";a:2:{s:4:"docs";s:1:"4";s:4:"hits";s:1:"4";}s:2:"it";a:2:{s:4:"docs";s:1:"1";s:4:"hits";s:1:"1";}}s:5:"query";s:7:"test it";}i:1;a:11:{s:5:"error";s:0:"";s:7:"warning";s:0:"";s:6:"status";i:0;s:6:"fields";a:1:{i:0;s:4:"body";}s:5:"attrs";a:2:{s:8:"group_id";i:1;s:5:"@expr";i:5;}s:7:"matches";a:4:{i:111;a:2:{s:6:"weight";s:1:"1";s:5:"attrs";a:2:{s:8:"group_id";s:1:"1";s:5:"@expr";d:-1;}}i:222;a:2:{s:6:"weight";s:1:"1";s:5:"attrs";a:2:{s:8:"group_id";s:1:"1";s:5:"@expr";d:-1;}}i:333;a:2:{s:6:"weight";s:1:"1";s:5:"attrs";a:2:{s:8:"group_id";s:1:"2";s:5:"@expr";d:-1;}}i:444;a:2:{s:6:"weight";s:1:"4";s:5:"attrs";a:2:{s:8:"group_id";s:1:"1";s:5:"@expr";d:-4;}}}s:5:"total";s:1:"4";s:11:"total_found";s:1:"4";s:4:"time";s:5:"0.002";s:5:"words";a:2:{s:4:"test";a:2:{s:4:"docs";s:1:"4";s:4:"hits";s:1:"4";}s:2:"it";a:2:{s:4:"docs";s:1:"1";s:4:"hits";s:1:"1";}}s:5:"query";s:7:"test it";}}}

test/test_16/test.xml
@@ -1,0 +1,63 @@
+<?xml version="1.0" encoding="utf-8"?>
+<test>
+
+<name>expr sorting vs. filters</name>
+
+<config>
+indexer
+{
+	mem_limit		= 16M
+}
+
+searchd
+{
+	<searchd_settings/>
+}
+
+source srctest
+{
+	type			= mysql
+	<sql_settings/>
+	sql_query		= SELECT document_id, group_id, body FROM test_table
+	sql_attr_uint	= group_id
+}
+index test
+{
+	source			= srctest
+	path			= <data_path/>/test
+	charset_type	= utf-8
+}
+</config>
+
+<query_attributes>
+<attr>group_id</attr>
+<attr>@expr</attr>
+</query_attributes>
+
+<queries>
+<query mode="any" filter="group_id" filtervalue="1" sortmode="expr" sortby="@weight">test it</query>
+<query mode="any" sortmode="expr" sortby="-@weight">test it</query>
+</queries>
+
+<db_create>
+CREATE TABLE `test_table`
+(
+	`document_id` int(11) NOT NULL default '0',
+	`group_id` int(11) NOT NULL default '0',
+	`body` varchar(255) NOT NULL default ''
+)
+</db_create>
+
+<db_drop>
+DROP TABLE IF EXISTS `test_table`
+</db_drop>
+
+<db_insert>
+INSERT INTO `test_table` VALUES
+( 111, 1, 'this is test' ),
+( 222, 1, 'just a test' ),
+( 333, 2, 'for test-ing purposes' ),
+( 444, 1, 'lets test it' )
+</db_insert>
+
+</test>

commit 32109c13672e77781f8fd679ddecf84b5b85dca9

fixed @geodist vs full scan broken in r1152 (suboptimal now; needs to be made cleaner)
git-svn-id: http://sphinxsearch.googlecode.com/svn/trunk@1153 8b96e2b9-35c5-2c16-bc47-5122d61876d4

src/sphinx.cpp
@@ -11907,6 +11907,7 @@ void CSphIndex_VLN::MatchFullScan ( const CSphQuery * pQuery, int iSorters, ISph
 		{
 			tMatch.m_iDocID = DOCINFO2ID(pDocinfo);
 			CopyDocinfo ( tMatch, pDocinfo );
+			EarlyCalc ( tMatch );
 
 			if ( EarlyReject ( tMatch, pQuery ) )
 				continue;