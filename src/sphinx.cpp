nzinfo/csft

commit fca06cde6d316aa9915d99b498b728f63a78ab79

fixed crash vs orphaned MVA values (bug #442)
git-svn-id: http://sphinxsearch.googlecode.com/svn/trunk@2097 8b96e2b9-35c5-2c16-bc47-5122d61876d4

src/sphinx.cpp
@@ -1573,7 +1573,7 @@ private:
 	const DWORD *				FindDocinfo ( SphDocID_t uDocID ) const;
 	void						CopyDocinfo ( CSphQueryContext * pCtx, CSphMatch & tMatch, const DWORD * pFound ) const;
 
-	bool						BuildMVA ( const CSphVector<CSphSource*> & dSources, CSphAutoArray<CSphWordHit> & dHits, int iArenaSize, int iFieldFD, int nFieldMVAs, int iFieldMVAInPool, AttrIndexBuilder_c * pMinMax );
+	bool						BuildMVA ( const CSphVector<CSphSource*> & dSources, CSphAutoArray<CSphWordHit> & dHits, int iArenaSize, int iFieldFD, int nFieldMVAs, int iFieldMVAInPool );
 
 	CSphDict *					SetupStarDict ( CSphScopedPtr<CSphDict> & tContainer, ISphTokenizer & tTokenizer ) const;
 	CSphDict *					SetupExactDict ( CSphScopedPtr<CSphDict> & tContainer, CSphDict * pPrevDict, ISphTokenizer & tTokenizer ) const;
@@ -8147,7 +8147,7 @@ struct MvaEntryCmp_fn
 
 bool CSphIndex_VLN::BuildMVA ( const CSphVector<CSphSource*> & dSources,
 		CSphAutoArray<CSphWordHit> & dHits, int iArenaSize, int iFieldFD,
-		int nFieldMVAs, int iFieldMVAInPool, AttrIndexBuilder_c * pMinMax )
+		int nFieldMVAs, int iFieldMVAInPool )
 {
 	// initialize writer (data file must always exist)
 	CSphWriter wrMva;
@@ -8330,8 +8330,6 @@ bool CSphIndex_VLN::BuildMVA ( const CSphVector<CSphSource*> & dSources,
 		{
 			if ( uCurID )
 			{
-				if ( pMinMax )
-					pMinMax->CollectMVA ( uCurID, dCurInfo );
 				wrMva.PutDocid ( uCurID );
 				ARRAY_FOREACH ( i, dCurInfo )
 				{
@@ -8374,9 +8372,6 @@ bool CSphIndex_VLN::BuildMVA ( const CSphVector<CSphSource*> & dSources,
 			return false;
 		}
 	}
-
-	if ( pMinMax )
-		pMinMax->FinishCollect ( true );
 
 	// clean up readers
 	ARRAY_FOREACH ( i, dBins )
@@ -9544,17 +9539,12 @@ int CSphIndex_VLN::Build ( const CSphVector<CSphSource*> & dSources, int iMemory
 
 	PROFILE_END ( collect_hits );
 
-	// prepare the collector for min/max of attributes
-	AttrIndexBuilder_c tMinMax ( m_tSchema );
-	CSphVector<DWORD> dMinMaxBuffer ( tMinMax.GetExpectedSize ( m_tStats.m_iTotalDocuments ) );
-	tMinMax.Prepare ( &dMinMaxBuffer[0] );
-
 	///////////////////////////////////////
 	// collect and sort multi-valued attrs
 	///////////////////////////////////////
 
 	if ( !BuildMVA ( dSources, dHits, iHitsMax*sizeof(CSphWordHit),
-		fdTmpFieldMVAs.GetFD (), nFieldMVAs, iMaxPoolFieldMVAs, &tMinMax ) )
+		fdTmpFieldMVAs.GetFD (), nFieldMVAs, iMaxPoolFieldMVAs ) )
 		return 0;
 
 	// reset hits pool
@@ -9718,6 +9708,14 @@ int CSphIndex_VLN::Build ( const CSphVector<CSphSource*> & dSources, int iMemory
 		pDocinfo = dDocinfos;
 		SphDocID_t uLastId = 0;
 		m_uMinMaxIndex = 0;
+
+		// prepare the collector for min/max of attributes
+		AttrIndexBuilder_c tMinMax ( m_tSchema );
+		CSphVector<DWORD> dMinMaxBuffer ( tMinMax.GetExpectedSize ( m_tStats.m_iTotalDocuments ) );
+		CSphVector < CSphVector<DWORD> > dCurInfo;
+		tMinMax.Prepare ( &dMinMaxBuffer[0] );
+		dCurInfo.Resize ( dMvaIndexes.GetLength() );
+
 		while ( qDocinfo.GetLength() )
 		{
 			// obtain bin index and next entry
@@ -9765,8 +9763,15 @@ int CSphIndex_VLN::Build ( const CSphVector<CSphSource*> & dSources, int iMemory
 						ARRAY_FOREACH ( i, dMvaIndexes )
 						{
 							sphSetRowAttr ( DOCINFO2ATTRS(pEntry), dMvaLocators[i], SphAttr_t(rdMva.GetPos()/sizeof(DWORD)) ); // intentional clamp; we'll check for 32bit overflow later
-							rdMva.SkipBytes ( rdMva.GetDword()*sizeof(DWORD) );
+
+							DWORD iMvaCount = rdMva.GetDword();
+							dCurInfo[i].Resize ( 0 );
+							dCurInfo[i].Reserve ( iMvaCount );
+							while ( iMvaCount-- )
+								dCurInfo[i].Add( rdMva.GetDword() );
 						}
+
+						tMinMax.CollectMVA ( uMvaID, dCurInfo );
 
 						uMvaID = rdMva.GetDocid();
 						if ( !uMvaID )

test/test_99/model.bin
@@ -1,0 +1,1 @@
+a:1:{i:0;a:4:{i:0;a:13:{s:5:"error";s:0:"";s:7:"warning";s:0:"";s:6:"status";i:0;s:6:"fields";a:1:{i:0;s:4:"text";}s:5:"attrs";a:2:{s:4:"mva1";i:1073741825;s:4:"mva2";i:1073741825;}s:7:"matches";a:1:{i:1;a:2:{s:6:"weight";s:1:"1";s:5:"attrs";a:2:{s:4:"mva1";a:1:{i:0;s:1:"1";}s:4:"mva2";a:2:{i:0;s:1:"1";i:1;s:1:"2";}}}}s:5:"total";s:1:"1";s:11:"total_found";s:1:"1";s:4:"time";s:5:"0.003";s:5:"words";a:1:{s:5:"test1";a:2:{s:4:"docs";s:1:"1";s:4:"hits";s:1:"1";}}s:8:"resarray";i:0;s:8:"roundoff";i:0;s:5:"query";s:5:"test1";}i:1;a:13:{s:5:"error";s:0:"";s:7:"warning";s:0:"";s:6:"status";i:0;s:6:"fields";a:1:{i:0;s:4:"text";}s:5:"attrs";a:2:{s:4:"mva1";i:1073741825;s:4:"mva2";i:1073741825;}s:7:"matches";a:1:{i:511;a:2:{s:6:"weight";s:1:"1";s:5:"attrs";a:2:{s:4:"mva1";a:1:{i:0;s:3:"511";}s:4:"mva2";a:4:{i:0;s:3:"510";i:1;s:3:"511";i:2;s:3:"512";i:3;s:3:"513";}}}}s:5:"total";s:1:"1";s:11:"total_found";s:1:"1";s:4:"time";s:5:"0.003";s:5:"words";a:1:{s:7:"test511";a:2:{s:4:"docs";s:1:"1";s:4:"hits";s:1:"1";}}s:8:"resarray";i:0;s:8:"roundoff";i:0;s:5:"query";s:7:"test511";}i:2;a:13:{s:5:"error";s:0:"";s:7:"warning";s:0:"";s:6:"status";i:0;s:6:"fields";a:1:{i:0;s:4:"text";}s:5:"attrs";a:2:{s:4:"mva1";i:1073741825;s:4:"mva2";i:1073741825;}s:7:"matches";a:1:{i:2048;a:2:{s:6:"weight";s:1:"1";s:5:"attrs";a:2:{s:4:"mva1";a:1:{i:0;s:4:"2048";}s:4:"mva2";a:1:{i:0;s:4:"2049";}}}}s:5:"total";s:1:"1";s:11:"total_found";s:1:"1";s:4:"time";s:5:"0.003";s:5:"words";a:1:{s:8:"test2048";a:2:{s:4:"docs";s:1:"1";s:4:"hits";s:1:"1";}}s:8:"resarray";i:0;s:8:"roundoff";i:0;s:5:"query";s:8:"test2048";}i:3;a:13:{s:5:"error";s:0:"";s:7:"warning";s:0:"";s:6:"status";i:0;s:6:"fields";a:1:{i:0;s:4:"text";}s:5:"attrs";a:2:{s:4:"mva1";i:1073741825;s:4:"mva2";i:1073741825;}s:7:"matches";a:1:{i:4000;a:2:{s:6:"weight";s:1:"1";s:5:"attrs";a:2:{s:4:"mva1";a:1:{i:0;s:4:"4000";}s:4:"mva2";a:1:{i:0;s:4:"4001";}}}}s:5:"total";s:1:"1";s:11:"total_found";s:1:"1";s:4:"time";s:5:"0.003";s:5:"words";a:1:{s:8:"test4000";a:2:{s:4:"docs";s:1:"1";s:4:"hits";s:1:"1";}}s:8:"resarray";i:0;s:8:"roundoff";i:0;s:5:"query";s:8:"test4000";}}}

commit 43956e127cac5aef2c04a4cba799daf4eeebd316

fixed a crash in block indexing (bug #447; broken since r2097)

src/sphinx.cpp
@@ -6875,7 +6875,8 @@ private:
 	DWORD						m_uStride;		// size of attribute's chunk (in DWORDs)
 	DWORD						m_uElements;	// counts total number of collected min/max pairs
 	int							m_iLoop;		// loop inside one set
-	DWORD*						m_pOutBuffer;	// storage for collected min/max
+	DWORD *						m_pOutBuffer;	// storage for collected min/max
+	DWORD *						m_pOutMax;		// storage max for bound checking
 	SphDocID_t					m_uStart;		// first and last docids of current chunk
 	SphDocID_t					m_uLast;
 	SphDocID_t					m_uIndexStart;	// first and last docids of whole index
@@ -6910,6 +6911,10 @@ private:
 		DWORD * pMinAttrs = DOCINFO2ATTRS(pMinEntry);
 		DWORD * pMaxEntry = pMinEntry + m_uStride;
 		DWORD * pMaxAttrs = pMinAttrs + m_uStride;
+
+		assert ( pMaxEntry < m_pOutMax );
+		assert ( pMaxAttrs < m_pOutMax );
+
 		m_uIndexLast = m_uLast;
 
 		DOCINFOSETID ( pMinEntry, m_uStart );
@@ -6959,6 +6964,7 @@ public:
 		, m_uElements ( 0 )
 		, m_iLoop ( 0 )
 		, m_pOutBuffer ( NULL )
+		, m_pOutMax ( NULL )
 		, m_uStart ( 0 )
 		, m_uLast ( 0 )
 		, m_uIndexStart ( 0 )
@@ -7005,9 +7011,10 @@ public:
 		m_dMvaIndexMax.Resize ( m_dMvaAttrs.GetLength() );
 	}
 
-	void Prepare ( DWORD * pOutBuffer )
+	void Prepare ( DWORD * pOutBuffer, DWORD * pOutMax )
 	{
 		m_pOutBuffer = pOutBuffer;
+		m_pOutMax = pOutMax;
 		m_uElements = 0;
 		m_uIndexStart = m_uIndexLast = 0;
 		ARRAY_FOREACH ( i, m_dIntIndexMin )
@@ -7134,6 +7141,9 @@ public:
 		CSphRowitem * pMinAttrs = DOCINFO2ATTRS(pMinEntry);
 		CSphRowitem * pMaxAttrs = DOCINFO2ATTRS(pMaxEntry);
 
+		assert ( pMaxEntry < m_pOutMax );
+		assert ( pMaxAttrs < m_pOutMax );
+
 		DOCINFOSETID ( pMinEntry, m_uIndexStart );
 		DOCINFOSETID ( pMaxEntry, m_uIndexLast );
 
@@ -7180,7 +7190,7 @@ bool CSphIndex_VLN::PrecomputeMinMax()
 		return true;
 
 	AttrIndexBuilder_c tBuilder ( m_tSchema );
-	tBuilder.Prepare ( m_pDocinfoIndex );
+	tBuilder.Prepare ( m_pDocinfoIndex, m_pDocinfoIndex + 2*( 1+m_uDocinfoIndex )*( DOCINFO_IDSIZE + m_tSchema.GetRowSize() ) );
 
 	DWORD uStride = DOCINFO_IDSIZE + m_tSchema.GetRowSize();
 	DWORD uProgressEntry = 0;
@@ -9712,9 +9722,8 @@ int CSphIndex_VLN::Build ( const CSphVector<CSphSource*> & dSources, int iMemory
 		// prepare the collector for min/max of attributes
 		AttrIndexBuilder_c tMinMax ( m_tSchema );
 		CSphVector<DWORD> dMinMaxBuffer ( tMinMax.GetExpectedSize ( m_tStats.m_iTotalDocuments ) );
-		CSphVector < CSphVector<DWORD> > dCurInfo;
-		tMinMax.Prepare ( &dMinMaxBuffer[0] );
-		dCurInfo.Resize ( dMvaIndexes.GetLength() );
+		CSphDocMVA tCurInfo ( dMvaIndexes.GetLength() );
+		tMinMax.Prepare ( &dMinMaxBuffer[0], &dMinMaxBuffer[0] + dMinMaxBuffer.GetLength()  );
 
 		while ( qDocinfo.GetLength() )
 		{
@@ -9741,7 +9750,6 @@ int CSphIndex_VLN::Build ( const CSphVector<CSphSource*> & dSources, int iMemory
 				}
 				iOrd++;
 				m_uMinMaxIndex += iDocinfoStride;
-				tMinMax.CollectWithoutMvas ( pEntry, false );
 
 				// update MVA
 				if ( bGotMVA )
@@ -9765,19 +9773,21 @@ int CSphIndex_VLN::Build ( const CSphVector<CSphSource*> & dSources, int iMemory
 							sphSetRowAttr ( DOCINFO2ATTRS(pEntry), dMvaLocators[i], SphAttr_t(rdMva.GetPos()/sizeof(DWORD)) ); // intentional clamp; we'll check for 32bit overflow later
 
 							DWORD iMvaCount = rdMva.GetDword();
-							dCurInfo[i].Resize ( 0 );
-							dCurInfo[i].Reserve ( iMvaCount );
+							tCurInfo.m_dMVA[i].Reserve ( iMvaCount );
 							while ( iMvaCount-- )
-								dCurInfo[i].Add( rdMva.GetDword() );
+								tCurInfo.m_dMVA[i].Add( rdMva.GetDword() );
 						}
-
-						tMinMax.CollectMVA ( uMvaID, dCurInfo );
 
 						uMvaID = rdMva.GetDocid();
 						if ( !uMvaID )
 							uMvaID = DOCID_MAX;
 					}
 				}
+
+				tMinMax.Collect ( pEntry, tCurInfo );
+
+				ARRAY_FOREACH ( i, tCurInfo.m_dMVA )
+					tCurInfo.m_dMVA[i].Resize( 0 );
 
 				// emit it
 				memcpy ( pDocinfo, pEntry, iDocinfoStride*sizeof(DWORD) );
@@ -10756,7 +10766,7 @@ bool CSphIndex_VLN::Merge ( CSphIndex * pSource, CSphVector<CSphFilterSettings> 
 		AttrIndexBuilder_c tMinMax ( m_tSchema );
 		CSphVector<DWORD> dMinMaxBuffer ( tMinMax.GetExpectedSize (
 			m_tStats.m_iTotalDocuments + pSrcIndex->GetStats().m_iTotalDocuments ) );
-		tMinMax.Prepare ( &dMinMaxBuffer[0] );
+		tMinMax.Prepare ( &dMinMaxBuffer[0], &dMinMaxBuffer[0] + dMinMaxBuffer.GetLength() );
 		m_uMinMaxIndex = 0;
 
 		DWORD * pSrcRow = pSrcIndex->m_pDocinfo.GetWritePtr(); // they *can* be null if the respective index is empty